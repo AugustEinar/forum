dcc0a4aca0f2c9fef7dfd11dff8090b5c40f03ed
agust <virusvorn1@hotmail.com>
2017-09-05 15:21:01 +0000

    heimaverk1

---
+++ resources/views/threads/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create a New Thread</div>
+
+                <div class="panel-body">
+                <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                            </div>
+                        </div>
+
+                </div>
+                <div class="panel panel-default">
+                </div>
+                    <div class="panel-body">
+               
+                      
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
